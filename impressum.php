8e4e5cb5a43e8918b3807867674b9837c5ae5be2
sempt3x <32197873+sempt3x@users.noreply.github.com>
2018-10-14 18:14:08 +0200

    Update impressum.php
    Code Formatierung

--- impressum.php
+++ impressum.php
@@ -1,49 +1,69 @@
 <?php ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Impressum</title>
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <title>Impressum</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M"
+        crossorigin="anonymous">
 </head>
+
 <body>
-<div class="container">
-                
-      
+    <div class="container">
 
-        <div class="row">
-        <div class="col-md-12 col-sm-12">
-        	<a class="h4" href="index.php"><- Zurück</a>
-                    <hr>
-        <h2>Impressum</h2>
-        <hr>
+
+
         <div class="row">
             <div class="col-md-12 col-sm-12">
-                Denis Zielonka<br> Kleine Str. 2<br> 76549 Hügelsheim<br> Deutschland
-                <br>
-                <br>
-                <br> Telefon: 072295149
-                <br>
-                <br> E-Mail: info@denis-zielonka.com
-                <br>
-                <br> Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit, die Sie hier finden https://ec.europa.eu/consumers/odr/. Zur Teilnahme an einem Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle sind wir nicht verpflichtet und nicht bereit.<br>
+                <a class="h4" href="index.php">
+                    <- Zurück</a> <hr>
+                        <h2>Impressum</h2>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                Denis Zielonka<br> Kleine Str. 2<br> 76549 Hügelsheim<br> Deutschland
+                                <br>
+                                <br>
+                                <br> Telefon: 072295149
+                                <br>
+                                <br> E-Mail: info@denis-zielonka.com
+                                <br>
+                                <br> Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS)
+                                bereit, die Sie hier finden https://ec.europa.eu/consumers/odr/. Zur Teilnahme an einem
+                                Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle sind wir nicht
+                                verpflichtet und nicht bereit.<br>
+                            </div>
+
+                        </div>
+                        <hr>
+                        <h2>Haftungshinweis:</h2>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+
+                                <br> Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die
+                                Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren
+                                Betreiber verantwortlich. Wir bemühen uns stets um die Aktualität unserer
+                                Internetseiten. Sollte dennoch einmal ein Fehler auftreten, so übernehmen wir hierfür
+                                keine Haftung. Mit Urteil vom 12.5.1998 hat das Landgericht Hamburg entschieden, dass
+                                man durch die Ausbringung eines Links die Inhalte der verlinkten URL ggf. mit zu
+                                veranworten hat. Dies kann, so das Landgericht, nur dadurch verhindert werden, dass man
+                                sich ausdrücklich von den verlinkten Inhalten der Seiten distanziert, was wir hiermit
+                                ausdrücklich tun. Wir haben auf allen unseren Seiten Links zu anderen Homepages gelegt.
+                                Für alle diese Links gilt folgendes: Wir betonen ausdrücklich, dass wir keinerlei
+                                Einfluss auf die Gestaltung und die Inhalte der verlinkten Seiten haben. Diese
+                                Erklärung gilt ausdrücklich auch für Seiten mit pornografischen und rassistischen
+                                Inhalten, da wir Prüfungen der eingetragenen Seiten nur stichprobenartig vornehmen
+                                können. Sollten Sie solche Seiten finden bitten wir um Mitteilung.
+                            </div>
+
+                        </div>
+
             </div>
 
-        </div>
-        <hr>
-        <h2>Haftungshinweis:</h2>
-        <hr>
-        <div class="row">
-            <div class="col-md-12 col-sm-12">
-                
-                <br> Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich. Wir bemühen uns stets um die Aktualität unserer Internetseiten. Sollte dennoch einmal ein Fehler auftreten, so übernehmen wir hierfür keine Haftung. Mit Urteil vom 12.5.1998 hat das Landgericht Hamburg entschieden, dass man durch die Ausbringung eines Links die Inhalte der verlinkten URL ggf. mit zu veranworten hat. Dies kann, so das Landgericht, nur dadurch verhindert werden, dass man sich ausdrücklich von den verlinkten Inhalten der Seiten distanziert, was wir hiermit ausdrücklich tun. Wir haben auf allen unseren Seiten Links zu anderen Homepages gelegt. Für alle diese Links gilt folgendes: Wir betonen ausdrücklich, dass wir keinerlei Einfluss auf die Gestaltung und die Inhalte der verlinkten Seiten haben. Diese Erklärung gilt ausdrücklich auch für Seiten mit pornografischen und rassistischen Inhalten, da wir Prüfungen der eingetragenen Seiten nur stichprobenartig vornehmen können. Sollten Sie solche Seiten finden bitten wir um Mitteilung.
-            </div>
 
         </div>
-
-        </div>
-
-        
-      </div>
-      </div>
+    </div>
 </body>
+
 </html>
