c420e16ff405a44f4734966c02db8d7a358362a9
sempt3x <32197873+sempt3x@users.noreply.github.com>
2018-10-14 17:45:52 +0200

    Test
    Test

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
 	<link href="css/custom.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 </head>
-
 <body>
 	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 		<a class="navbar-brand" href="#">Denis Zielonka</a>
