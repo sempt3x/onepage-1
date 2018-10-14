dc9806eb4d5abe18088a81fb9cf049c007416de4
sempt3x <32197873+sempt3x@users.noreply.github.com>
2018-10-14 04:03:56 +0200

    Balken Diagram in die Skills Section eingefügt. Erfahrungen Inhalt eingefügt. Kontakt Formular hergerichtet mit Icons zu verschiedenen Platformen rechts Footer überarbeitet -> Copyright, Datenschutz und Impressum.

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 		<link href="css/custom.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 	</head>
 	<body>
 		<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
@@ -47,15 +48,17 @@
 			<div class="row h1 text-center">
 				<div class="col-md-12 text">
 				  	Denis Zielonka
+				  	<hr>
 				</div>
 			</div>
 		</div>
 		</div>
-		<div class="uebermich pb-4" id="uebermich">
+		<div class="uebermich p-4" id="uebermich">
 			<div class="container">
 				<div class="row text-center">
 					<div class="col-md-12 h1 p-4">
 						Über mich
+						<hr>
 					</div>
 				</div>
 			</div>
@@ -65,68 +68,105 @@
 					  	Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
 					</div>
 					<div class="col-md-6 text-right">
-					  	Bild
+					  	<div class="profil">
+					  		
+					  	</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="portfolio" id="portfolio">
+		<div class="portfolio p-4" id="portfolio">
 			<div class="container">
 				<div class="row text-center">
 					<div class="col-md-12 h1 p-4">
 						Portfolio
+						<hr>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="skills" id="skills">
+		<div class="skills p-4" id="skills">
 			<div class="container">
 				<div class="row text-center">
 					<div class="col-md-12 h1 p-4">
 						Skills
+          <hr>
+          <div class="progress mt-4"><div class="progress-bar bg-success" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 100%"> HTML</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-success" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 100%"> CSS</div></div>
+         <div class="progress mt-4"><div class="progress-bar bg-success" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%"> PHP</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 80%"> JAVASCRIPT</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 72%"> Bootstrap</div></div>
+         <div class="progress mt-4"><div class="progress-bar bg-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 72%"> JQuery</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-info" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 71%"> ADOBE PHOTOSHOP CC</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-info" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 71%"> ADOBE DREAMWEAVER CC</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-info" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 71%"> ADOBE Premiere Pro CC</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-warning" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 55%"> WORDPRESS</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-warning" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 55%"> Debian</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-warning" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 55%"> Ubuntu</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-warning" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 55%"> Centos</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-danger" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 20%"> Joomla</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-danger" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 20%"> SaSS</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-danger" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 7%"> XML</div></div>
+          <div class="progress mt-4"><div class="progress-bar bg-danger" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" style="width: 7%"> Json</div></div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="erfahrung" id="erfahrung">
+		<div class="erfahrung p-4" id="erfahrung">
 			<div class="container">
 				<div class="row text-center">
 					<div class="col-md-12 h1 p-4">
 						Erfahrung
+						<hr>
+					</div>
+					<div class="text-left">
+						 <h5><span class="badge badge-secondary">Web Developer</span></h5>
+                            <p>Die Firma ITIC GmbH mit Firmensitz in Kuppenheim ist seit nun mehr als 10 Jahren im World Wide Web als kompetenter Ansprechpartner tätig. Was damals mit innovativen Kassen- und Bonussystemen und den Anfängern der Webprogrammierung begann, findet nun in optimierten und barrierefreien Internetauftritten seine Fortsetzung. Das Kerngeschäft bezieht sich auf Webdesign, Programmierung und Webhosting.
+                            </p>
+                
+                          <p>Meine Tätigkeiten umfassten vor allem die Programmierung von Internetseiten mit PHP, Javascript, HTML und CSS. Zu seinem Aufgabenbereich gehörten insbesondere:</p>
+                          <ul>
+                              <li>Grafische Entwürfe und Gestaltung von Webseiten in Adobe Photoshop CS5; Technische Umsetzung mit HTML, CSS, PHP, JavaScript</li>
+                              <li>Pflege von Webseiten sowohl mit als auch ohne CMS (Content Management System)</li>
+                              <li>Einführung von responsive Webdesign (benutzerfreundliche Darstellung von Webseiten auch auf mobilen Endgeräten)</li>
+                              <li>Schulung der Kunden in den verwendeten Content Management Systemen</li>
+                              <li>Erstellung von Newslettern in Frontpage</li>
+                          </ul>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="kontakt" id="kontakt">
+		<div class="kontakt p-4" id="kontakt">
 			<div class="container">
 				<div class="row text-center">
 					<div class="col-md-12 h1 p-4">
 						Kontakt
+						<hr>
 					</div>
 				</div>
 				<div class="row">
-						<div class="col-md-9">
+						<div class="col-md-7">
 							<form>
 								<div class="form-group row">
 									<label for="inputEmail3" class="col-sm-2 form-control-label">Email</label>
-									<div class="col-sm-5">
+									<div class="col-sm-8">
 										<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
 									</div>
 								</div>
 								<div class="form-group row">
 									<label for="inputBetreff" class="col-sm-2 form-control-label">Betreff</label>
-									<div class="col-sm-5">
+									<div class="col-sm-8">
 										<input type="text" class="form-control" id="inputBetreff" placeholder="Betreff">
 									</div>
 								</div>
 								<div class="form-group row">
 									<label for="inputText" class="col-sm-2 form-control-label">Text</label>
-									<div class="col-sm-5">
+									<div class="col-sm-8">
 										<textarea id="my-input" class="form-control" rows="3" wfd-id="91"></textarea>
 									</div>
 								</div>
 								<div class="form-group row">
-									<div class="col-sm-5">
+									<div class="col-sm-10">
 										<div class="checkbox">
 											<label>
 												<input required="" type="checkbox"> Ich habe die Datenschutzerklaerung gelesen und akzeptiert.
@@ -141,22 +181,28 @@
 								</div>
 							</form>
 						</div>
-						<div class="col-md-3">
-							
+						<div class="col-md-5 ">
+							<div class="plattformen" id="plattformen">
+									
+										<div class="text-left">
+											<ul>
+												<li><div class="fab fa-twitter fa-w-12 fa-4x"></div></li>
+												<li><div class="fab fa-linkedin fa-w-12 fa-4x"></div></li>
+												<li><div class="fab fa-github fa-w-12 fa-4x"></div></li>
+												<li><div class="fab fa-xing-square fa-w-12 fa-4x"></div></li>
+											</ul>
+										</div>					
+									
+							</div>
 						</div>
 					</div>
 			</div>
 		</div>
 		<footer>
-			<div>
-				
-			</div>
-			<div class="plattformen" id="plattformen">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12">
-							Test
-						</div>					
+			<div class="container">
+				<div class="row">
+				    <div class="col-md-12 text-center p-5 text-light">
+				    	Copyright by Denis Zielonka | <a href="datenschutzerklaerung.php">Datenschutzerklaerung</a> | <a href="impressum.php">Impressum</a>
 					</div>
 				</div>
 			</div>
