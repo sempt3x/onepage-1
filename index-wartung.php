f419bd6c9bd0a56dddc06a3f99af2b87b5ef7946
sempt3x <32197873+sempt3x@users.noreply.github.com>
2019-05-04 15:35:05 +0200

    Create index-wartung.php
    Eine zusätzliche Index.php erstellt -> die für Wartung eingesetzt wird.

---
+++ index-wartung.php
@@ -0,0 +1,43 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<meta http-equiv="Pragma" content="no-cache">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="../../../../favicon.ico">
+	<title>Denis Zielonka - Website (In Wartung)</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- jQuery first, then Popper.js and Bootstrap JS. -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
+	 crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M"
+	 crossorigin="anonymous">
+	<link href="css/custom.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz"
+	 crossorigin="anonymous">
+</head>
+
+<body>
+	<div class="vorschau" id="vorschau">
+		<div class="container">
+			<div class="row h1 text-center">
+				<div class="col-md-12 text">
+					Denis Zielonka
+					<hr>
+				</div>
+			</div>
+			<div class="row text-center">
+				<div>
+					Derzeit in Wartung!
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
